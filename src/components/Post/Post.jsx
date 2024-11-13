62cfb4b739606b73ec2c6114b83a59e2e15570ee
Anypogers <gnutzmannenzo@gmail.com>
2024-11-13 10:54:51 -0300

    commit

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -3,45 +3,8 @@ import styles from "./Post.module.css";
 
 export function Post() {
   return (
-    <article className={styles.post}>
-      <header>
-        <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/Anypogers.png"
-          />
-          <div className={styles.authorInfo}>
-            <strong>Anypogers</strong>
-            <span>Game Developer</span>
-          </div>
-        </div>
-        <time title="October 27th, 02:22am" dateTime="27/10/2024 02:22am">
-          Posted 4 days ago.
-        </time>
-      </header>
+    <section className={styles.category}>
       
-      <div className={styles.content}>
-        <p>Yooooo</p>
-        <p>Just started playing In Stars and Time, massive banger ngl.</p>
-        <img className={styles.postImage} src="https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/2x1_NSwitchDS_InStarsAndTime_image1600w.jpg" alt="In Stars and Time"/>
-        <p className={styles.tags}>
-          <a href="#">#gaming</a>
-          <a href="#">#instarsandtime</a>
-          <a href="#">#indiegames</a>
-        </p>
-      </div>
-
-      <form className={styles.commentForm}>
-        <strong>Leave a comment!</strong>
-        <textarea placeholder="Text goes here!"></textarea>
-        <footer>
-          <button type="submit">Post</button>
-        </footer>
-      </form>
-      <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-      </div>
-    </article>
+    </section>
   );
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -20,7 +20,36 @@ export function Topbar() {
         </footer>
       </div>
       <div className={styles.title}>
-        <strong>Anypoger's Posts</strong>
+        <div className={styles.word}>
+          <div className={styles.word_letter}><strong>A</strong></div>
+          <div className={styles.word_letter}><strong>n</strong></div>
+          <div className={styles.word_letter}><strong>y</strong></div>
+          <div className={styles.word_letter}><strong>p</strong></div>
+          <div className={styles.word_letter}><strong>o</strong></div>
+          <div className={styles.word_letter}><strong>g</strong></div>
+          <div className={styles.word_letter}><strong>e</strong></div>
+          <div className={styles.word_letter}><strong>r</strong></div>
+          <div className={styles.word_letter}><strong>'</strong></div>
+          <div className={styles.word_letter}><strong>s</strong></div>
+        </div>
+        <div className={styles.word}>
+          <div className={styles.word_letter}><strong>G</strong></div>
+          <div className={styles.word_letter}><strong>a</strong></div>
+          <div className={styles.word_letter}><strong>m</strong></div>
+          <div className={styles.word_letter}><strong>e</strong></div>
+        </div>
+        <div className={styles.word}>
+          <div className={styles.word_letter}><strong>S</strong></div>
+          <div className={styles.word_letter}><strong>u</strong></div>
+          <div className={styles.word_letter}><strong>g</strong></div>
+          <div className={styles.word_letter}><strong>e</strong></div>
+          <div className={styles.word_letter}><strong>s</strong></div>
+          <div className={styles.word_letter}><strong>t</strong></div>
+          <div className={styles.word_letter}><strong>i</strong></div>
+          <div className={styles.word_letter}><strong>o</strong></div>
+          <div className={styles.word_letter}><strong>n</strong></div>
+          <div className={styles.word_letter}><strong>s</strong></div>
+        </div>
       </div>
     </div>
   );
