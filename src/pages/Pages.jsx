55b1d1edc43490b2e9b128f48df3c15b6860de6c
Anypogers <gnutzmannenzo@gmail.com>
2025-04-15 12:30:05 -0300

    Initiated the portfolio page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import {Home} from './Home/Home';
 import {Games} from './Games/Games';
+import {Portfolio} from './Portfolio/Portfolio';
 import {NotFound} from './NotFound/NotFound';
 
 export function Pages() {
@@ -11,6 +11,7 @@ export function Pages() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<Games />} />
+        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,6 +1,24 @@
-export function NotFound() {
-    return (
-        <>
-        </>
-    )
+import styles from './Portfolio.module.css'
+
+export function Portfolio() {
+  return (
+    <>
+      <header>
+        Portfolio
+      </header>
+      <section>
+
+      </section>
+      <footer>
+        <section>
+          <div className={styles.footerGit}>
+            <a href="https://github.com/Anypogers">
+              <img src="https://github.com/Anypogers.png"/>
+              GitHub
+            </a>
+          </div>
+        </section>
+      </footer>
+    </>
+  )
 }
